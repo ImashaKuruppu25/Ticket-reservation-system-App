4047ffb5ebe50cf4ca096b6da3d60b660c485a08
Imasha Kuruppu <imasha@rooster.org>
2023-10-06 21:08:27 +0530

    get all schedule by passing search terms and pagination data

--- Ticket-reservation-system/Controllers/ScheduleController.cs
+++ Ticket-reservation-system/Controllers/ScheduleController.cs
@@ -118,5 +118,69 @@ namespace Ticket_reservation_system.Controllers
 
             return Ok(existingSchedule);
         }
+
+        [HttpGet()]
+        [Authorize(Roles = "Backoffice")]
+        public ActionResult<IEnumerable<ScheduleDto>> GetAllSchedules([FromQuery] int currentPage = 1, [FromQuery] int limit = 10, [FromQuery] string searchTerm = null)
+        {
+            var schedulesCollection = _mongoDBService.Schedules;
+
+            // Define a filter to search by schedule properties (modify as needed)
+            var searchFilter = Builders<Schedule>.Filter.Or(
+                Builders<Schedule>.Filter.Regex(s => s.TrainName, new MongoDB.Bson.BsonRegularExpression(searchTerm ?? "", "i")), // Case-insensitive search by Train name
+                Builders<Schedule>.Filter.Regex(s => s.StartingStation, new MongoDB.Bson.BsonRegularExpression(searchTerm ?? "", "i")) // Case-insensitive search by StartingStation
+            );
+
+            // Get the total count of schedules matching the filter
+            long totalCount = schedulesCollection.CountDocuments(searchFilter);
+
+            // Calculate the total number of pages
+            int totalPages = (int)((totalCount + limit - 1) / limit);
+
+            // Ensure the requested page is within bounds
+            if (currentPage < 1) currentPage = 1;
+            if (currentPage > totalPages) currentPage = totalPages;
+
+            // Calculate skip and limit values for pagination
+            int skip = (currentPage - 1) * limit;
+
+            // Retrieve the list of schedules based on the filter, skip, and limit
+            var schedules = schedulesCollection.Find(searchFilter)
+                .Skip(Math.Max(0, skip))
+                .Limit(limit)
+                .ToList();
+
+            // Map the Schedule objects to ScheduleDto objects using an anonymous type
+            var scheduleDtos = schedules.Select(schedule => new AllScheculeDto
+            {
+                Id = schedule.Id,
+                Type = schedule.Type,
+                TrainId = schedule.TrainId,
+                TrainName= schedule.TrainName,
+                Status = schedule.Status,
+                StartingStation = schedule.StartingStation,
+                DepartureTime = schedule.DepartureTime,
+                DepartureDate = schedule.DepartureDate,
+                Destinations = schedule.Destinations.Select(destination => new DestinationDto
+                {
+                    Name = destination.Name,
+                    ReachTime = destination.ReachTime,
+                    Price = destination.Price
+                }).ToList(),
+                AvailableTicketCount = schedule.AvailableTicketCount
+            });
+
+            // Return the list of schedules along with pagination information
+            var result = new
+            {
+                Page = currentPage,
+                PageSize = limit,
+                TotalCount = totalCount,
+                TotalPages = totalPages,
+                Data = scheduleDtos
+            };
+
+            return Ok(result);
+        }
     }
 }
---
+++ Ticket-reservation-system/Models/Dtos/AllScheduleDto.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+
+namespace Ticket_reservation_system.Models.Dtos
+{
+    public class AllScheculeDto
+    {
+        public required string Id { get; set; }
+        public required string Type { get; set; }
+        public required string TrainId { get; set; }
+        public required string TrainName { get; set; }
+        public required string Status { get; set; }
+        public required string StartingStation { get; set; }
+        public required TimeOnly DepartureTime { get; set; }
+        public required DateOnly DepartureDate { get; set; }
+        public List<DestinationDto> Destinations { get; set; }
+        public required int AvailableTicketCount { get; set; }
+    }
+
+}
